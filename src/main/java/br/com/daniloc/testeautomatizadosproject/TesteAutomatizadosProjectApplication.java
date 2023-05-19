34ae2a0d85fccf2628742813f355146d6675c585
chaimsohn <danilo_chaimsohn@hotmail.com>
2023-05-19 08:28:22 -0300

    Start do projeto para testes automatizados do curso SantanderCoders by Ada

---
+++ src/main/java/br/com/daniloc/testeautomatizadosproject/TesteAutomatizadosProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniloc.testeautomatizadosproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAutomatizadosProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteAutomatizadosProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/daniloc/testeautomatizadosproject/TesteAutomatizadosProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniloc.testeautomatizadosproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAutomatizadosProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
