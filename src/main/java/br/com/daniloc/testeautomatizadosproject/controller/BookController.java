30f3f42845ab29a5e1c541191e9673b2d5584527
chaimsohn <danilo_chaimsohn@hotmail.com>
2023-05-19 09:21:12 -0300

    Adicionando Controller do usuario e livro

---
+++ src/main/java/br/com/daniloc/testeautomatizadosproject/controller/BookController.java
@@ -0,0 +1,31 @@
+package br.com.daniloc.testeautomatizadosproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface BookController {
+
+    @PostMapping
+    ResponseEntity<Mono<Void>> save(@RequestBody BookRequest bookRequest);
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Mono<BookResponse>> find(@PathVariable String id);
+
+    @GetMapping(value = "/{isbn}")
+    ResponseEntity<Mono<BookResponse>> findByIsbn(@PathVariable String isbn);
+
+    @GetMapping
+    ResponseEntity<Flux<BookResponse>> findAll();
+
+    @PatchMapping(value = "/{id}")
+    ResponseEntity<Mono<BookResponse>> update(@PathVariable String id, @RequestBody BookRequest bookRequest);
+
+    @DeleteMapping(value = "/{id}")
+    ResponseEntity<Mono<Void>> delete(@PathVariable String id);
+
+    @DeleteMapping(value = "/{isbn}")
+    ResponseEntity<Mono<Void>> deleteByIsbn(@PathVariable String isbn);
+}
+
--- src/main/java/br/com/daniloc/testeautomatizadosproject/entity/Book.java
+++ src/main/java/br/com/daniloc/testeautomatizadosproject/entity/Book.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 @Data
 @Builder
 @Document
-public class Books {
+public class Book {
 
     @Id
     private String id;
