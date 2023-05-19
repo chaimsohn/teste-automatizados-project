3e283354c65071f987fcc082d2826fa004a44b2d
chaimsohn <danilo_chaimsohn@hotmail.com>
2023-05-19 08:57:06 -0300

    Adicionando entidades de livros e usuarios

---
+++ src/main/java/br/com/daniloc/testeautomatizadosproject/entity/Books.java
@@ -0,0 +1,26 @@
+package br.com.daniloc.testeautomatizadosproject.entity;
+
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@Document
+public class Books {
+
+    @Id
+    private String id;
+    private String title;
+    private String resume;
+    private String sumary;
+    private Double price;
+    @Indexed(unique = true)
+    private String isbn;
+    private LocalDate releaseDate;
+}
