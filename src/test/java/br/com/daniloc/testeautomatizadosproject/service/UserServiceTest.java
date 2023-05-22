200d39f7b6eb3aa30110ac1db38e24042cec3958
Patrícia da Silva Ramos  Jujuba <56210981+PatriciaRamosS@users.noreply.github.com>
2023-05-21 23:38:34 -0300

    add unit test to Update service

--- src/test/java/br/com/daniloc/testeautomatizadosproject/service/UserServiceTest.java
+++ src/test/java/br/com/daniloc/testeautomatizadosproject/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.daniloc.testeautomatizadosproject.service;
 
+import br.com.daniloc.testeautomatizadosproject.entity.Book;
 import br.com.daniloc.testeautomatizadosproject.entity.User;
 import br.com.daniloc.testeautomatizadosproject.mapper.UserMapper;
 import br.com.daniloc.testeautomatizadosproject.model.request.UserRequest;
@@ -71,5 +72,25 @@ class UserServiceTest {
                 .verify();
 
         Mockito.verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    void testUpdate() {
+
+        UserRequest request = new UserRequest("patricia", "patricia@gmail.com", "123", "23");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class), any(User.class))).thenReturn(entity);
+        when(repository.findById(anyString())).thenReturn(Mono.just(entity));
+        when(repository.save(any(User.class))).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.update("123", request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).save(any(User.class));
     }
 }
